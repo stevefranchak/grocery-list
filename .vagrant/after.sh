2cea40603a9af71df8c676ebadd025b4cb13e51d
Steve Franchak <stevefranchak@gmail.com>
2018-04-11 20:28:36 -0400

    Removed Vagrant files.

--- .vagrant/after.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# If you would like to do some extra provisioning you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
-
-sudo wget -O /usr/local/bin/rmate https://raw.github.com/aurora/rmate/master/rmate
-sudo chmod a+x /usr/local/bin/rmate
